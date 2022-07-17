b954a8f7accb8651a5df6c4a24752bcf7c803df8
hayannn <dlgkdis801@naver.com>
2022-07-17 18:10:31 +0900

    Update MainActivity.java

--- HoEAT/app/src/main/java/com/example/hoeattest/MainActivity.java
+++ HoEAT/app/src/main/java/com/example/hoeattest/MainActivity.java
@@ -8,24 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_id, tv_pass;
+//    private TextView tv_id, tv_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
+//        tv_id = findViewById(R.id.tv_id);
+//        tv_pass = findViewById(R.id.tv_pass);
 
 
 
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
+//        Intent intent = getIntent();
+//        String userID = intent.getStringExtra("userID");
+//        String userPass = intent.getStringExtra("userPass");
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
+//        tv_id.setText(userID);
+//        tv_pass.setText(userPass);
 
     }
 }
