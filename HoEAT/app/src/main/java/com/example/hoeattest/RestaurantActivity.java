4d395df82dcf75131600752c2888f5d32f92d85b
joonhwan-choi <83737075+joonhwan-choi@users.noreply.github.com>
2022-07-18 15:44:55 +0900

    .

---
+++ HoEAT/app/src/main/java/com/example/hoeattest/RestaurantActivity.java
@@ -0,0 +1,32 @@
+package com.example.hoeattest;
+
+import android.os.Bundle;
+import android.widget.TabHost;
+import androidx.appcompat.app.AppCompatActivity;
+public class RestaurantActivity extends AppCompatActivity {
+
+//    private TextView tv_id, tv_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabHost tab_host = (TabHost) findViewById(R.id.tabhost);
+        tab_host.setup();
+
+        TabHost.TabSpec ts1 = tab_host.newTabSpec("tab1");
+        ts1.setIndicator("tab1");
+        ts1.setContent(R.id.tab1);
+        tab_host.addTab(ts1);
+
+        TabHost.TabSpec ts2 = tab_host.newTabSpec("tab2");
+        ts2.setIndicator("tab2");
+        ts2.setContent(R.id.tab2);
+        tab_host.addTab(ts2);
+
+
+        tab_host.setCurrentTab(0);
+
+    }
+
+}
