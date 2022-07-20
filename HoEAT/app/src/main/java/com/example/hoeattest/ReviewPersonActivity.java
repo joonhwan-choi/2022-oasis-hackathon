83e86bdb7cbdd2b852eddf311940c7d8980355ab
hayannn <dlgkdis801@naver.com>
2022-07-21 00:51:39 +0900

    review_person Update

---
+++ HoEAT/app/src/main/java/com/example/hoeattest/ReviewPersonActivity.java
@@ -0,0 +1,16 @@
+package com.example.hoeattest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReviewPersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_person);
+    }
+}
+
