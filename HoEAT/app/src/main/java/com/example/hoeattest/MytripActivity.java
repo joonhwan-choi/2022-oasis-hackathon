a33c8d440398a0bfc14c86135c69aa1f9b0c5f99
joonhwan-choi <83737075+joonhwan-choi@users.noreply.github.com>
2022-07-20 23:41:41 +0900

    .

--- HoEAT/app/src/main/java/com/example/hoeattest/MytripActivity.java
+++ HoEAT/app/src/main/java/com/example/hoeattest/MytripActivity.java
@@ -1,6 +1,15 @@
 package com.example.hoeattest;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MytripActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mytrip);
+    }
 }
