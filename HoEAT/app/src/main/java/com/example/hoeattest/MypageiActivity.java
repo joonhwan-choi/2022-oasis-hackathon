2c4617a1eff7d1432ffec33e0c08d86051d670bd
joonhwan-choi <83737075+joonhwan-choi@users.noreply.github.com>
2022-07-21 01:46:19 +0900

    .

--- HoEAT/app/src/main/java/com/example/hoeattest/MypageiActivity.java
+++ HoEAT/app/src/main/java/com/example/hoeattest/MypageiActivity.java
@@ -4,24 +4,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MypageiActivity extends AppCompatActivity {
     //네비
     private ImageView nv_myp1,nv_myp2,nv_myp3;
+
+    private TextView mytripbutton,myreviewbutton, mylikebutton;
+
     //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypagei);
-
+        myreviewbutton=findViewById(R.id.myreviewbutton);
+        mytripbutton=findViewById(R.id.mytripbutton);
+        mylikebutton=findViewById(R.id.mylikebutton);
         //네비
         nv_myp1=findViewById(R.id.nv_myp1);
         nv_myp2=findViewById(R.id.nv_myp2);
         nv_myp3=findViewById(R.id.nv_myp3);
         //
+        mylikebutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        myreviewbutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyreviewActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mytripbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), MytripActivity.class);
+
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
 
         //채팅
         nv_myp1.setOnClickListener(new View.OnClickListener() {
--- HoEAT/app/src/main/java/com/example/hoeattest/MyreviewActivity.java
+++ HoEAT/app/src/main/java/com/example/hoeattest/MyreviewActivity.java
@@ -1,14 +1,30 @@
 package com.example.hoeattest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyreviewActivity extends AppCompatActivity {
-
+    Button writereviewButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myreview);
+        writereviewButton =findViewById(R.id.writereviewButton);
+
+
+        writereviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), Lik.class);
+//
+//                startActivity(intent);
+//                finish();
+            }
+        });
+
     }
 }
